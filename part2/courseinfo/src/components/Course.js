f8d3b2b9dfcb24852269be113673e16307fa232f
mecene <mecene.co@gmail.com>
2023-02-23 11:19:46 +0100

    courseInfo 7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -7,12 +7,21 @@ const Content = ({parts}) =>
        <>
        {parts.map(part => <Part  key={part.id} part={part}/>)} 
        </>
+// Component to render total of exercises 
+const Total = ({parts}) => {
+    let total = 0
+    parts.forEach(part => total += part.exercises) //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+    return(
+        total
+    )
+}
 
 const Course = ({course}) => {
     return (
         <div>
             <Header course={course} />
             <Content parts={course.parts}/>
+            <strong>Total of <Total parts={course.parts}/> exercises</strong>
         </div>
 
     )
