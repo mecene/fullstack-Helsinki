f5c2c8da43c28ceeb4d6374f66c159cdad4d1af9
mecene <mecene.co@gmail.com>
2023-02-23 10:57:13 +0100

    courseinfo6

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,38 +1,21 @@
+// Component header
 const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Part = ({ part }) =>{
-    console.log(part);
-    return(
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    )
-}
-
-
-const Content = ({ parts }) => {
-
-    return(
-        parts.map(value => <Part   part={value}/>) /// ====
-    )
-
-    
-    
-}
-
-
-
+// Component to render single course details
+const Part = ({ part }) =><p>{part.name} {part.exercises}</p>
+// Component to render collection of courses
+const Content = ({parts}) => 
+       <>
+       {parts.map(part => <Part  key={part.id} part={part}/>)} 
+       </>
 
 const Course = ({course}) => {
     return (
         <div>
             <Header course={course} />
-            {/* <Content parts={course.parts} /> */}
+            <Content parts={course.parts}/>
         </div>
 
     )
 }
-
-
-
+// export Es6 module 
 export default Course
