6e4c4fa1d0f795ecc6be85c7e868684536256974
mecene <mecene.co@gmail.com>
2023-02-23 21:15:41 +0100

    step9

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -10,19 +10,36 @@ const Content = ({ parts }) =>
 // Component to render total of exercises 
 const Total = ({ parts }) => {
 
-    return (parts.reduce((s, p) => s + p.exercises, 0)) // accumulator is the value returned from the previous iteration
+    return (parts.reduce((s, p) => s + p.exercises, 0))
+
 
 }
 
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header course={course} />
-            <Content parts={course.parts} />
-            <strong>Total of <Total parts={course.parts} /> exercises</strong>
-        </div>
+const Course = ({ courses }) => {
 
+    // const course = courses.map(element => <Header course={element} />)
+    //     console.log(course);
+
+    return (
+        <>
+
+            {courses.map((course) => {
+                //console.log(course.name);
+                return (
+                    <>
+                        <Header key={course.id} course={course} />
+                        <Content key={course.parts.id} parts={course.parts} />
+                        <strong>Total of <Total parts={course.parts} /> exercises</strong>
+                    </>
+                )
+
+            })}
+        </>
     )
 }
+
+
+
+
 // export Es6 module 
 export default Course
