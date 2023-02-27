e860b60d172dfe084c8ca5e592c717b1eb08bfb7
mecene <mecene.co@gmail.com>
2023-02-27 11:34:29 +0100

    part-2_phonebook_step-5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '004474444444444'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+
 
   const handleNewName = (e) => setNewName(e.target.value)
   const handleNewNumber = (e) => setNewNumber(e.target.value)
+  const handleChange = (e) => {
+    // find the person that includes the inputs value 
+    setSearch(e.target.value.toLowerCase())
+  }
 
   const addPerson = (e) => {
     e.preventDefault()
     const newObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      //id: persons.length + 1
     }
     //filter trough array to find value
     const alreadyExist = persons.filter(person => person.name === newName).length > 0
@@ -28,10 +34,19 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
+  //conditionel of collection to show if searched
+  const personsToShow = search.length > 0
+    ? persons.filter(person => person.name.toLowerCase().includes(search))
+    : persons
+    
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with:
+        <input type="text" onChange={handleChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNewName} value={newName} />
@@ -45,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
 
     </div>
