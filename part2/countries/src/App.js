dcf149331498c522365cd82ee4411619340a08b8
mecene <mecene.co@gmail.com>
2023-03-19 21:14:14 +0100

    data-for-countries-step-2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -27,6 +27,13 @@ function App() {
 
   // set the state of search to the input value (lower case)
   const handleChange = (e) => setSearch(e.target.value.toLowerCase())
+  // retrieve the input tag from DOM 
+  const searchInput = document.getElementsByTagName('input')
+  // update search state value && insert value to input
+  const handleClick = (countryName) => {
+    setSearch(countryName.toLowerCase())
+    searchInput[0].value = countryName
+  }
 
   //conditionel of collection to show if searched
   const countriesToShow = search.length > 0
@@ -44,41 +51,38 @@ function App() {
         return ' Too many results'
       }
       if (searchLength === 1) {
-        {
-          return (
-            countriesToShow.map((country, index) =>
-              <div key={index}>
-                <h2> {country.name.common} </h2>
-                <p>Capital : {country.capital}</p>
-                <p>Area : {country.area}</p>
-                <h3>Languages</h3>
-                <ul>
-                  {Object.values(country.languages).map((value, index) => (
-                    <li key={index}>
-                      {value}
-                    </li>
-                  )
-                  )}
-                </ul>
-                <img src={country.flags.png} alt={country.flag} />
-              </div>
-
-            )
+        return (
+          countriesToShow.map((country, index) =>
+            <div key={index}>
+              <h2> {country.name.common} </h2>
+              <p>Capital : {country.capital}</p>
+              <p>Area : {country.area}</p>
+              <h3>Languages</h3>
+              <ul>
+                {Object.values(country.languages).map((value, index) => (
+                  <li key={index}>
+                    {value}
+                  </li>
+                )
+                )}
+              </ul>
+              <img src={country.flags.png} alt={country.flag} />
+            </div>
           )
-
-        }
+        )
       }
+
       return (
         //console.log(countriesToShow)
         <ul>
           {countriesToShow.map((country, index) =>
             <li key={index}>
               {country.name.common}
+              <button onClick={() => handleClick(country.name.common)}>show</button>
             </li>
+
           )}
         </ul>
-
-
       )
     }
 
